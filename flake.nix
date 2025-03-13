8f0b2785920a65545a5ca5b12c93d4f7c273a5d3
Paweł Chojnacki <pawelchcki@gmail.com>
2025-03-13 23:18:21 +0100

    w

--- flake.nix
+++ flake.nix
@@ -7,113 +7,108 @@
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , systems
-    , treefmt-nix
-    , pre-commit-hooks
-    ,
-    }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-      python3 = forAllSystems (system: pkgs.${system}.python312);
-      python3Packages = forAllSystems (system: python3.${system}.pkgs.pythonPackages);
-      treefmtEval = forAllSystems (system: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} ./treefmt.nix);
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    treefmt-nix,
+    pre-commit-hooks,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    python3 = forAllSystems (system: pkgs.${system}.python312);
+    python3Packages = forAllSystems (system: python3.${system}.pkgs.pythonPackages);
+    treefmtEval = forAllSystems (system: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} ./treefmt.nix);
 
-      rebuildr = forAllSystems (
-        system: attrs:
-          python3.${system}.pkgs.buildPythonApplication {
-            pname = "rebuildr";
-            version = "0.1";
+    rebuildr = forAllSystems (
+      system: attrs:
+        python3.${system}.pkgs.buildPythonApplication {
+          pname = "rebuildr";
+          version = "0.1";
 
-            inherit (attrs) doCheck;
+          inherit (attrs) doCheck;
 
-            src = ./.;
-            format = "pyproject";
-
-            build-system = with python3Packages.${system}; [
-              hatchling
-            ];
-
-            checkInputs = with python3Packages.${system}; [
-              pytestCheckHook
-            ];
-
-            propagatedBuildInputs = with pkgs.${system}; [
-              skopeo
-            ];
-          }
-      );
-    in
-    {
-      packages = forAllSystems (system: {
-        default = rebuildr.${system} {
-          doCheck = false;
-        };
-
-        docker-image = pkgs.${system}.dockerTools.buildImage {
-          name = "rebuildr";
-          tag = "latest";
-
-          copyToRoot = pkgs.${system}.buildEnv {
-            name = "image-root";
-            paths = [ self.packages.${system}.default ];
-            pathsToLink = [ "/bin" ];
-          };
-
-          config = {
-            Cmd = [ "/bin/rebuildr" ];
-            WorkingDir = "/";
-          };
-        };
-      });
-
-      formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
-
-      checks = forAllSystems (system: {
-        default = rebuildr.${system} {
-          doCheck = true;
-        };
-
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
           src = ./.;
-          hooks = {
-            # nixpkgs-fmt.enable = true;
-            treefmt.package = treefmtEval.${system}.config.build.wrapper;
-            treefmt.enable = true;
-          };
+          format = "pyproject";
+
+          build-system = with python3Packages.${system}; [
+            hatchling
+          ];
+
+          checkInputs = with python3Packages.${system}; [
+            pytestCheckHook
+          ];
+
+          propagatedBuildInputs = with pkgs.${system}; [
+            skopeo
+          ];
+        }
+    );
+  in {
+    packages = forAllSystems (system: {
+      default = rebuildr.${system} {
+        doCheck = false;
+      };
+
+      docker-image = pkgs.${system}.dockerTools.buildImage {
+        name = "rebuildr";
+        tag = "latest";
+
+        copyToRoot = pkgs.${system}.buildEnv {
+          name = "image-root";
+          paths = [self.packages.${system}.default];
+          pathsToLink = ["/bin"];
         };
-      });
 
-      devShells = forAllSystems (system:
-        let
-          _pkgs = pkgs.${system};
-        in
-        {
-          default = _pkgs.mkShellNoCC {
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
+        config = {
+          Cmd = ["/bin/rebuildr"];
+          WorkingDir = "/";
+        };
+      };
+    });
 
-            packages = with _pkgs; [
-              uv
-              git
-            ];
+    formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
 
-            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+    checks = forAllSystems (system: {
+      default = rebuildr.${system} {
+        doCheck = true;
+      };
 
-            env = {
-              # Don't create venv using uv
-              UV_NO_SYNC = "1";
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          # nixpkgs-fmt.enable = true;
+          treefmt.package = treefmtEval.${system}.config.build.wrapper;
+          treefmt.enable = true;
+        };
+      };
+    });
 
-              # Force uv to use Python interpreter from venv
-              UV_PYTHON = "${python3.${system}}/bin/python";
+    devShells = forAllSystems (system: let
+      _pkgs = pkgs.${system};
+    in {
+      default = _pkgs.mkShellNoCC {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
 
-              # Prevent uv from downloading managed Python's
-              UV_PYTHON_DOWNLOADS = "never";
-            };
-          };
-        });
-    };
+        packages = with _pkgs; [
+          uv
+          git
+        ];
+
+        buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+
+        env = {
+          # Don't create venv using uv
+          UV_NO_SYNC = "1";
+
+          # Force uv to use Python interpreter from venv
+          UV_PYTHON = "${python3.${system}}/bin/python";
+
+          # Prevent uv from downloading managed Python's
+          UV_PYTHON_DOWNLOADS = "never";
+        };
+      };
+    });
+  };
 }
