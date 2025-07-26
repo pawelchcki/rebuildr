128523cceadf9d0e4e3beb029bd62ba96d48f4b3
Paweł Chojnacki <pawelchcki@gmail.com>
2025-07-26 23:25:25 +0200

    wip

---
+++ bzl/example/apps/realworld-gin-sqlc/realworld.rebuildr.py
@@ -0,0 +1,27 @@
+from rebuildr.descriptor import (
+    Descriptor,
+    GitHubCommitInput,
+    Inputs,
+    ImageTarget,
+)
+
+image = Descriptor(
+    inputs=Inputs(
+        external=[
+            GitHubCommitInput(
+                owner="aliml92",
+                repo="realworld-gin-sqlc",
+                commit="f01271b55086265c3e07191fff469f4b902ecf96",
+                target_path=".",
+            ),
+        ],
+    ),
+    targets=[
+        ImageTarget(
+            repository="ghcr.io/aliml92/realworld-gin-sqlc",
+            tag="v0",
+            dockerfile="Dockerfile",
+            platform="linux/amd64",
+        ),
+    ],
+)
